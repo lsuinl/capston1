71a8e661ca8d5046035137db01deae49f888d3f6
lsuinl <su96in43@naver.com>
2025-05-26 17:47:58 +0900

    로그아웃 아이콘 변경

--- lib/main/component/setting_menu.dart
+++ lib/main/component/setting_menu.dart
@@ -15,48 +15,24 @@ class SettingMenu extends ConsumerStatefulWidget {
 class _SettingMenuState extends ConsumerState<SettingMenu> {
   @override
   Widget build(BuildContext context) {
-    return PopupMenuButton<String>(
-      shadowColor: SIDE_COLOR,
-      color: SIDE_COLOR,
-      position: PopupMenuPosition.under,
-      itemBuilder: (context) {
-        return [
-          _menuItem("로그아웃"),
-        ];
-      },
-      onSelected: (value) {},
+    return IconButton(onPressed: () async {
+      final storage = ref.read(secureStorageProvider);
+      await storage.delete(key: ACCESS_TOKEN_KEY);
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: const Text('로그아웃되었습니다.'),
+          ),
+        );
+        Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(builder: (_) => const LoginScreen()),
+              (route) => false,
+        );
+      }
+    },
       constraints: const BoxConstraints(minWidth: 50, maxWidth: 150),
-      offset: const Offset(-20, 20),
-      icon: const Icon(Icons.settings,color: Colors.white,),
+      icon: const Icon(Icons.logout, color: Colors.white,),
       iconSize: 30,
-      enabled: true,
-    );
-  }
-
-  PopupMenuItem<String> _menuItem(String text) {
-    return PopupMenuItem<String>(
-      enabled: true,
-      onTap: () async {
-        final storage = ref.read(secureStorageProvider);
-        await storage.delete(key: ACCESS_TOKEN_KEY);
-        if (mounted) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: const Text('로그아웃되었습니다.'),
-            ),
-          );
-          Navigator.of(context).pushAndRemoveUntil(
-            MaterialPageRoute(builder: (_) => const LoginScreen()),
-            (route) => false,
-          );
-        }
-      },
-      value: text,
-      height: 50,
-      child: Text(
-        text,
-        style: const TextStyle(color: Colors.black),
-      ),
     );
   }
 }
