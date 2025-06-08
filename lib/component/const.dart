c9df55c309518057ed3bc95583f48f372b3e8112
lsuinl <su96in43@naver.com>
2025-06-09 00:36:22 +0900

    서버 주소 변경

--- lib/component/const.dart
+++ lib/component/const.dart
@@ -8,11 +8,7 @@ Color SIDE_COLOR = Color(0xFFECE6F0);
 const String ACCESS_TOKEN_KEY = 'access_token';
 const String USER_ID = 'refresh_token';
 
-//localhost
-final emulatorIp='10.0.2.2:3000';
-final simulatorIp='127.0.0.1:3000';
-
-String ip = "localhost:3000";
+String ip = "52.79.81.133:80";
 
 List<String> answer = [
   '자유롭게 질문해보세요!\n여러분의 주체성 향상을 위한 코멘트를 드릴게요!',
